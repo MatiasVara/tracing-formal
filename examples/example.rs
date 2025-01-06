dac2238dc03a30c2ce62aa5897d528fda6103bb9
Matias Ezequiel Vara Larsen <matiasevara@torokernel.io>
2025-01-06 19:47:35 +0100

    Add example of events in a type
    Signed-off-by: Matias Ezequiel Vara Larsen <matiasevara@torokernel.io>

--- examples/example.rs
+++ examples/example.rs
@@ -1,6 +1,8 @@
 use solver::Alternates;
 use tracing::instrument;
 use tracing_formal::TracingFormal;
+use tracing::event;
+use tracing::Level;
 
 #[instrument(fields(event = "do_hola"))]
 fn hola() {
@@ -8,6 +10,7 @@ fn hola() {
 }
 
 // TODO: this does not have context
+// TODO: fields can store parameters to the functions and the result
 #[instrument(fields(event = "do_chau"))]
 fn chau() {
     println!("do chau()");
@@ -15,13 +18,19 @@ fn chau() {
 
 fn main() {
     // `do_hola`` and `do_chau`` events must satisfy an alternate relation between them
+    // this is the spec that should be translated to rust
     let alternates: Alternates = Alternates::new("do_hola", "do_chau");
 
     let subscriber = TracingFormal::new(vec![alternates]);
 
     tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
 
-    hola();
+    // just an event as example
+    // events provide better granularity
+    // fields is used to trigger the `do_hola` event
+    event!(Level::INFO, event="do_hola");
+
+    hola(); // this is a violation of the partial order
     chau();
     chau(); // this is a violation in the partial order
 }
---
+++ examples/example_struct.rs
@@ -0,0 +1,38 @@
+use solver::Alternates;
+use tracing::instrument;
+use tracing_formal::TracingFormal;
+
+#[derive(Debug)]
+struct MyType {
+   name: &'static str,
+}
+
+// impl a type and define events 
+impl MyType {
+    #[instrument(fields(event = "do_hola"))]
+    pub fn hola(self) {
+         println!("hola from {}", self.name);
+    }
+
+    #[instrument(fields(event = "do_chau"))]
+    pub fn chau(self){
+        println!("chau from {}", self.name);
+    }
+}
+
+fn main() {
+    // `do_hola`` and `do_chau`` events must satisfy an alternate relation between them
+    // this is the spec that should be translated to rust
+    let alternates: Alternates = Alternates::new("do_hola", "do_chau");
+
+    let subscriber = TracingFormal::new(vec![alternates]);
+
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
+
+    let test = MyType {
+        name: "MyTypeInstance"
+    };
+    
+    // the following line triggers a violation 
+    test.chau();
+}
